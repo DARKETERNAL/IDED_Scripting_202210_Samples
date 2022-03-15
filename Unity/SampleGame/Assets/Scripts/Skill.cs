c1dea76a32e9b645ed8e56404f6009eaf4a38218
Andres Perez Campillo <tomailsandstuff@gmail.com>
2022-03-15 07:35:30 -0500

    Update Skill.cs

--- Unity/SampleGame/Assets/Scripts/Skill.cs
+++ Unity/SampleGame/Assets/Scripts/Skill.cs
@@ -1,18 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class Skill : MonoBehaviour
+public abstract class Skill
 {
-    // Start is called before the first frame update
-    void Start()
+    //private string name;
+
+    //public string Name
+    //{
+    //    get => name;
+    //    protected set => name = value;
+    //}
+
+    public string Name { get; protected set; }
+
+    public virtual float EffectValue { get; protected set; }
+
+    public Skill()
     {
-        
+        Name = "Unused";
+        EffectValue = 0F;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Skill(string name, float effectValue)
     {
-        
+        Name = name;
+        EffectValue = effectValue;
+    }
+}
+
+public class AttackSkill : Skill
+{
+    public AttackSkill() : base()
+    {
+    }
+
+    public AttackSkill(string name, float effectValue) : base(name, effectValue)
+    {
+    }
+}
+
+public abstract class SupportSkill : Skill
+{
+    public SupportSkill() : base()
+    {
+    }
+
+    public SupportSkill(string name, float effectValue) : base(name, effectValue)
+    {
+    }
+}
+
+public class DefuffSkill : SupportSkill
+{
+    public float EffectChance { get; protected set; }
+
+    public override float EffectValue
+    {
+        get => base.EffectValue;
+        protected set => base.EffectValue = Math.Max(Math.Abs(value), 0.75F);
+    }
+
+    public DefuffSkill() : base()
+    {
+        EffectChance = 1F;
+    }
+
+    public DefuffSkill(string name, float effectValue, float effectChance) : base(name, effectValue)
+    {
+        EffectChance = Math.Abs(effectChance);
+    }
+}
+
+public class HealSkill : SupportSkill
+{
+    public override float EffectValue
+    {
+        get => base.EffectValue;
+        protected set => base.EffectValue = Math.Min(1F, (float)Math.Round(value));
+    }
+
+    public HealSkill()
+    {
+        Name = "Cure";
+        EffectValue = 10F;
+    }
+
+    public HealSkill(string name, float effectValue) : base(name, effectValue)
+    {
     }
 }
